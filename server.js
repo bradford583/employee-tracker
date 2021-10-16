7bd0a0bf7488d2281a27f66df033e0e974ded4d7
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-10-15 18:36:20 -0600

    deleted stuff that was not being used

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const mysql = require("mysql");
 //port designation
 const PORT = process.env.PORT || 3003;
 const app = express();
+
+const empTable = `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.department_name, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+      FROM employee 
+      LEFT JOIN manager ON employee.manager_id = manager.id
+      LEFT JOIN roles ON employee.role_id = roles.id 
+      LEFT JOIN department ON employee.department_id = department.id`;
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -38,148 +45,42 @@ function startServer() {
     .then(function (response) {
       switch (response.start) {
         case "View all employees":
-          let sql = `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.department_name, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
-          FROM employee 
-          LEFT JOIN manager ON employee.manager_id = manager.id
-          LEFT JOIN roles ON employee.role_id = roles.id 
-          LEFT JOIN department ON employee.department_id = department.id`;
-          db.query(sql, function (err, rows) {
-            if (err) throw err;
-            console.table(rows);
-            startServer();
-          });
+          viewEmps();
+          break;
+        //   let sql = `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.department_name, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+        //   FROM employee 
+        //   LEFT JOIN manager ON employee.manager_id = manager.id
+        //   LEFT JOIN roles ON employee.role_id = roles.id 
+        //   LEFT JOIN department ON employee.department_id = department.id`;
+        //   db.query(sql, function (err, rows) {
+        //     if (err) throw err;
+        //     console.table(rows);
+        //     startServer();
+        //   });
       }
     });
 }
 
-// Get all employees
-app.get("/api/employees", (req, res) => {
-  const sql = `SELECT employee.*, parties.name
-              AS party_name
-              FROM candidates
-              LEFT JOIN parties
-              ON candidates.party_id = parties.id`;
-
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: rows,
-    });
-  });
-});
-
-//Get single employee
-app.get("/api/employee/:id", (req, res) => {
-  const sql = `SELECT employee.*, parties.name
-              AS party_name
-              FROM candidates
-              LEFT JOIN parties
-              ON candidates.party_id = parties.id
-              WHERE candidates.id = ?`;
-
-  db.query(sql, params, (err, row) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: row,
-    });
-  });
-});
-
-//Delete a employee
-app.delete("/api/employee/:id", (req, res) => {
-  const sql = `DELETE FROM employee WHERE id = ?`;
-  const params = [req.params.id];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.statusMessage(400).json({ error: err.message });
-    } else if (!result.affectedRows) {
-      res.json({
-        message: "Employee not found",
+const viewEmps = () => {
+  // switch (response.start) {
+  //   case "View all employees":
+      db.query(empTable, function (err, rows) {
+        if (err) throw err;
+        console.table(rows);
+        startServer();
       });
-    } else {
-      res.json({
-        message: "deleted",
-        changes: result.affectedRows,
-        id: req.params.id,
-      });
-    }
-  });
-});
-
-//Create a candidate
-app.post("/api/employee", ({ body }, res) => {
-  const errors = inputCheck(
-    body,
-    "first_name",
-    "last_name",
-    "industry_connected"
-  );
-  if (errors) {
-    res.status(400).json({ error: errors });
-    return;
   }
 
-  const sql = `INSERT INTO employee (first_name, last_name, industry_connected)
-  VALUES (?, ?, ?)`;
-  const params = [body.first_name, body.last_name, body.industry_connected];
 
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: body,
-    });
-  });
-});
 
-app.get("/api/parties", (req, res) => {
-  const sql = `SELECT * FROM parties`;
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: rows,
-    });
-  });
-});
-
-app.get("/api/party/:id", (req, res) => {
-  const sql = `SELECT * FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, row) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: row,
-    });
-  });
-});
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 startServer();
